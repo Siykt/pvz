a76cc3e5fc40bf493a9905a8f95e77fc4fcb768a
siykt <cnsiykt@163.com>
2022-07-02 22:15:42 +0800

    feat: 添加背景音乐及控制组件

---
+++ src/views/game/modules/Music.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref, Ref } from 'vue'
+import MountainsAudio from '@/assets/mountains.ogg'
+
+interface Props {
+  /** 自动播放背景音乐 */
+  autoPlay?: boolean
+}
+
+type HTMLAudioElementRef = Ref<HTMLAudioElement | undefined>
+
+withDefaults(defineProps<Props>(), {
+  autoPlay: true
+})
+
+const BGMAudioRef: HTMLAudioElementRef = ref()
+
+const createMusicControl = (audioElmRef: HTMLAudioElementRef) => ({
+  play: () => audioElmRef.value?.play(),
+  pause: () => audioElmRef.value?.pause()
+})
+
+defineExpose({ BGMControl: createMusicControl(BGMAudioRef) })
+</script>
+<template>
+  <!-- 背景音乐 -->
+  <audio ref="BGMAudioRef" loop :src="MountainsAudio" :autoplay="autoPlay" />
+</template>
+
+<style lang="less" scoped>
+// something
+</style>
