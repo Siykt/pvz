5ede4c52c038099d5956ee5b879f980b29231738
siykt <cnsiykt@163.com>
2022-07-04 00:46:05 +0800

    feat: 添加植物选择栏组件

---
+++ src/views/game/modules/PlantChooseBar.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { useGameStoreRefs } from '@/store/game'
+import Diagram from '@/components/Plant/Diagram.vue'
+
+const { playerData } = useGameStoreRefs()
+</script>
+<template>
+  <div class="plant-choose-bar">
+    <div class="sun-count fc">{{ playerData.sun }}</div>
+    <div class="plant-list f">
+      <Diagram v-for="(plant, i) in playerData.plants" :key="i" :plant-diagram="plant.diagram" :sun="plant.sun" />
+    </div>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.plant-choose-bar {
+  position: absolute;
+  top: 10px;
+  left: 0;
+  width: 402px;
+  height: 75px;
+  background-image: url('@/assets/images/plant-choose-bar-bg.png');
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  background-position-x: -40px;
+  .sun-count {
+    position: absolute;
+    left: 20px;
+    bottom: 4px;
+    width: 50px;
+    height: 18px;
+    color: #000;
+    font-size: 14px;
+    // background-color: #000;
+  }
+  .plant-list {
+    padding-left: 79px;
+    padding-top: 5px;
+  }
+}
+</style>
