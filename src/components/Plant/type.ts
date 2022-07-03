e5e57731044b155475b2bb2b2005e3975973da1e
siykt <cnsiykt@163.com>
2022-07-04 00:46:29 +0800

    feat: 添加Plant类型系统

---
+++ src/components/Plant/type.ts
@@ -0,0 +1,17 @@
+export interface Bullet {
+  /** 子弹图片地址 */
+  src: string
+  /** 武器伤害 */
+  damage: number
+}
+
+export interface Plant {
+  /** 静态表示图地址 */
+  diagram: string
+  /** 动态表示图地址 */
+  gif: string
+  /** 子弹类型 */
+  bullet: Bullet
+  /** 需要多少太阳 */
+  sun: number
+}
