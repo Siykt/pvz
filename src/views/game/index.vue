a4cd59b00a70b5424f150ba1e53dd6bf2357c347
siykt <cnsiykt@163.com>
2022-07-04 00:51:13 +0800

    feat: 添加游戏页面并完善部分功能

---
+++ src/views/game/index.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { useGameStoreRefs } from '@/store/game'
+import { ref } from 'vue'
+import Layout from './modules/Layout.vue'
+import Music from './modules/Music.vue'
+import Menu from './modules/Menu.vue'
+import PlantChooseBar from './modules/PlantChooseBar.vue'
+import { Plant1, Plant2 } from '@/components/Plant'
+
+const { playerData } = useGameStoreRefs()
+const MusicRef = ref<InstanceType<typeof Music>>()
+const handleClick = () => {
+  // 解决自动播放的问题
+  MusicRef.value?.ChoosePlantBGMControl.play()
+}
+
+const init = () => {
+  playerData.value.plants = [Plant1, Plant2, Plant2, Plant2, Plant2]
+}
+
+init()
+</script>
+<template>
+  <div class="full" @click="handleClick">
+    <!--  -->
+    <Layout>
+      <Menu />
+      <PlantChooseBar />
+    </Layout>
+    <!-- 音乐控件 -->
+    <Music ref="MusicRef" :auto-play="false" />
+  </div>
+</template>
+
+<style lang="less" scoped></style>
