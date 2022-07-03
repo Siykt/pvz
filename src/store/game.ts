5cc58956f17d270cf5da49975cb6900b1b0d037c
siykt <cnsiykt@163.com>
2022-07-03 23:21:21 +0800

    feat: 添加Menu及其调整状态功能

--- src/store/game.ts
+++ src/store/game.ts
@@ -4,8 +4,6 @@ import { defineStore } from 'pinia'
 export enum GameStatus {
   /** 选择植物阶段 */
   choosePlant,
-  /** 游戏暂停 */
-  pause,
   /** 游戏中 */
   playing,
   /** 游戏结束 */
@@ -13,16 +11,21 @@ export enum GameStatus {
 }
 
 export interface GameStore {
+  /** 游戏状态 */
   gameStatus: GameStatus
+  /** 是否暂停 */
+  isPause: boolean
+  /** 是否禁用音乐 */
+  isDisabledMusic: boolean
+  /** 是否开启自动收集阳光 */
+  isAutoCollectingSun: boolean
 }
 
 export const useGameStore = defineStore('GameStore', {
   state: (): GameStore => ({
-    gameStatus: GameStatus.choosePlant
-  }),
-  actions: {
-    updateGameStatus(status: GameStatus) {
-      this.gameStatus = status
-    }
-  }
+    gameStatus: GameStatus.choosePlant,
+    isPause: false,
+    isDisabledMusic: false,
+    isAutoCollectingSun: false
+  })
 })
---
+++ src/views/game/modules/Menu.vue
@@ -0,0 +1,106 @@
+<script setup lang="ts">
+import { GameStatus, useGameStore } from '@/store/game'
+import { storeToRefs } from 'pinia'
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import MenuButton from './MenuButton.vue'
+
+const { gameStatus, isPause, isDisabledMusic, isAutoCollectingSun } = storeToRefs(useGameStore())
+const router = useRouter()
+
+const showDialog = ref(false)
+const handleShowMenu = () => {
+  showDialog.value = isPause.value = true
+}
+const handleReplay = () => {
+  gameStatus.value = GameStatus.choosePlant
+  handleContinueGame()
+}
+const handleContinueGame = () => {
+  isPause.value = showDialog.value = false
+}
+const handleToGameBegin = () => {
+  router.push('/')
+}
+</script>
+<template>
+  <div class="fc menu">
+    <MenuButton @click="handleShowMenu">菜单</MenuButton>
+  </div>
+  <transition name="fade">
+    <div v-show="showDialog" class="menu-dialog fc full">
+      <img class="menu-dialog-bg" src="@/assets/images/menu-dialog-bg.png" alt="menu-dialog-bg">
+      <div class="menu-dialog-content fcsb">
+        <div class="menu-dialog-options fcc">
+          <MenuButton class="btn" @click="handleReplay">重新开始</MenuButton>
+          <MenuButton class="btn" @click="handleToGameBegin">返回菜单</MenuButton>
+          <!-- 开启/关闭音乐 -->
+          <MenuButton
+            class="btn"
+            @click="isDisabledMusic = !isDisabledMusic"
+          >{{ isDisabledMusic ? '开启' : '关闭' }}音乐</MenuButton>
+          <!-- 开启/关闭自动收集阳光 -->
+          <MenuButton
+            class="btn"
+            @click="isAutoCollectingSun = !isAutoCollectingSun"
+          >{{ isAutoCollectingSun ? '关闭' : '开启' }}自动收集阳光</MenuButton>
+        </div>
+        <MenuButton class="btn" @click="handleContinueGame">继续游戏</MenuButton>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<style lang="less" scoped>
+.menu {
+  position: absolute;
+  right: 20px;
+  top: 20px;
+  z-index: 2;
+  .mr-2 {
+    margin-right: 2px;
+  }
+}
+.menu-dialog {
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.3);
+  z-index: 9;
+  &-bg {
+    position: absolute;
+    width: 412px;
+    height: 483px;
+    z-index: 1;
+  }
+  &-content {
+    position: relative;
+    width: 250px;
+    height: 483px;
+    z-index: 2;
+    padding: 120px 0 20px;
+    box-sizing: border-box;
+  }
+  .menu-dialog-options {
+    width: 80%;
+    .btn {
+      margin-bottom: 6px;
+      height: 50px;
+      font-size: 17px;
+    }
+  }
+  .btn {
+    width: 100%;
+    height: 65px;
+    font-size: 20px;
+  }
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
