c8d83f220ff9075c35078925d1d6520cfb74c6f4
siykt <cnsiykt@163.com>
2022-07-08 23:06:19 +0800

    feat: 添加跟随鼠标效果

--- src/views/game/modules/Layout.vue
+++ src/views/game/modules/Layout.vue
@@ -1,21 +1,45 @@
 <script setup lang="ts">
-import { ref, onMounted, onDeactivated, computed } from 'vue'
+import { ref, onMounted, onDeactivated, computed, unref } from 'vue'
 import { setScaleToResize } from '@/utils/setScale'
-import { GameStatus, useGameStore } from '@/store/game'
+import { GameStatus, useGameStoreRefs } from '@/store/game'
 
-const gameLayout = ref<HTMLDivElement>()
+const { gameStatus, playerData } = useGameStoreRefs()
+
+let scale: number
 let clear: () => void
-onMounted(() => (clear = setScaleToResize(gameLayout.value as HTMLDivElement, 900, 600)))
+const gameLayout = ref<HTMLDivElement>()
+const plantInstanceRef = ref<HTMLImageElement>()
+const gameContentBGPositionX = computed(() => (gameStatus.value === GameStatus.choosePlant ? '-500px' : '-115px'))
+
+const getPlantInstancePosition = ({ x, y }: MouseEvent) => {
+  return {
+    left: x / scale + 'px',
+    top: y / scale + 'px'
+  }
+}
+const handleMovePlant = (evn: MouseEvent) => {
+  const el = unref(plantInstanceRef)
+  if (!playerData.value.selectedPlant || !el) return
+  const { top, left } = getPlantInstancePosition(evn)
+  el.style.top = top
+  el.style.left = left
+}
+
+onMounted(() => ({ clear, scale } = setScaleToResize(gameLayout.value as HTMLDivElement, 900, 600)))
 onDeactivated(() => clear && clear())
-
-const gameStore = useGameStore()
-
-const gameContentBGPositionX = computed(() => (gameStore.gameStatus === GameStatus.choosePlant ? '-500px' : '-115px'))
 </script>
 <template>
-  <div class="full game-layout">
+  <div class="full game-layout" @mousemove="handleMovePlant">
     <div ref="gameLayout" :style="{ backgroundPositionX: gameContentBGPositionX }" class="full game-content">
       <slot />
+      <img
+        v-if="playerData.selectedPlant"
+        ref="plantInstanceRef"
+        class="plant-instance"
+        :src="playerData.selectedPlant.diagram"
+        :style="getPlantInstancePosition(playerData.selectedPlant.event)"
+        alt="plant-instance"
+      >
     </div>
   </div>
 </template>
@@ -28,6 +52,14 @@ const gameContentBGPositionX = computed(() => (gameStore.gameStatus === GameStat
     background: url('@/assets/images/game-layout-bg.jpg') no-repeat;
     background-size: 1400px 600px;
     transition: all 1s;
+  }
+
+  .plant-instance {
+    position: absolute;
+    transform: translate(-50%, -100%);
+    will-change: top left;
+    z-index: 2;
+    cursor: grab;
   }
 }
 </style>
